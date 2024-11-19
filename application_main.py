65bad678991913e9b6e1aa04d8a5068c6ae68bae
kav-een <kaveenla@gmail.com>
2024-11-19 20:49:08 +0530

    new modification made

--- application_main.py
+++ application_main.py
@@ -10,9 +10,6 @@ if __name__ == '__main__':
     print("Hello World...!")
     job_run_env = sys.argv[1]
 
-    print("Creating Spark Session")
-    spark = Utils.get_spark_session(job_run_env)
-
     
     orders_df = DataReader.read_orders(spark,job_run_env)
     orders_filtered = DataManipulation.filter_closed_orders(orders_df)
